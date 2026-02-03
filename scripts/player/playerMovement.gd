57d40609eb8d1e0c99e924a64b9d96a028f8161c
trevor-chi <trevor.childers@vikings.berry.edu>
2026-02-03 13:42:56 -0500

    implemented basic player movement and removed tester nodes

--- scripts/player/playerMovement.gd
+++ scripts/player/playerMovement.gd
@@ -1,15 +1,39 @@
 extends CharacterBody2D
 
-@export var speed := 200.0
+@onready var anim := $AnimatedSprite2D
 
-func _physics_process(_delta):
+@export var speed := 400.0
+@export var jumpVel := 600.0
+@export var gravity := 1200.0
+
+func _ready():
+	anim.play("Idle")
+
+func play_anim(name: String):
+	if anim.animation != name:
+		anim.play(name)
+
+func _physics_process(delta):
+	# gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# jump
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jumpVel
+
+	# horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed
+
+	# animations
+	if not is_on_floor():
+		play_anim("Jump")
+		anim.flip_h = direction < 0
+	elif direction != 0:
+		play_anim("Running")
+		anim.flip_h = direction < 0
+	else:
+		play_anim("Idle")
+
 	move_and_slide()
-	
-	if Input.is_action_pressed("move_left"):
-		print("LEFT")
-	
-	if Input.is_action_pressed("move_right"):
-		print("RIGHT")
-	
