48dfa1bece2eb6a67ce6ef1774fa5d676d9b1c6e
trevor-chi <trevor.childers@vikings.berry.edu>
2026-02-05 13:46:17 -0500

    Changed tileset asset, adjusted player movement logic and did some world building

--- scripts/player/playerMovement.gd
+++ scripts/player/playerMovement.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var speed := 400.0
 @export var jumpVel := 600.0
 @export var gravity := 1200.0
+var facing_dir := 1  # 1 = right, -1 = left
 
 func _ready():
 	anim.play("Idle")
@@ -24,16 +25,19 @@ func _physics_process(delta):
 
 	# horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		facing_dir = direction
+
 	velocity.x = direction * speed
 
 	# animations
 	if not is_on_floor():
 		play_anim("Jump")
-		anim.flip_h = direction < 0
 	elif direction != 0:
 		play_anim("Running")
-		anim.flip_h = direction < 0
 	else:
 		play_anim("Idle")
+
+	anim.flip_h = facing_dir < 0
 
 	move_and_slide()
